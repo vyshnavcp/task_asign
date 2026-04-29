66e1ffcc75b81f941a356fe910984f203d876299
vyshnavcp <vyshnavcp01@gmail.com>
2026-04-29 17:21:44 +0530

    update

--- myapp/templatetags/time_filters.py
+++ myapp/templatetags/time_filters.py
@@ -7,10 +7,13 @@ def duration_format(value):
     """Converts a timedelta to 'Xh Ym Zs' format."""
     if not value:
         return "-"
+
     total_seconds = int(value.total_seconds())
     if total_seconds < 0:
         total_seconds = 0
+
     h = total_seconds // 3600
     m = (total_seconds % 3600) // 60
     s = total_seconds % 60
-    return f"{h}h {m}m {s}s"
+
+    return f"{h}h {m:02d}m {s:02d}s"
